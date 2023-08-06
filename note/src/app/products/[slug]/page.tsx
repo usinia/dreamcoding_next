195a33f749d3d82e16e16c4403c319d4857635df
usinia <usinia0103@gmail.com>
2023-08-06 19:42:06 +0900

    [#1] 4.9 페이지 미리 생성

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -10,3 +10,11 @@ export default function PantsPage({ params }: Props) {
   console.log("props", params);
   return <h1>{params.slug} 제품 설명 페이지</h1>;
 }
+
+export function generateStaticParams() {
+  // dynamic route page 에서 특정 경로에 대해 먼저 렌더링 해두고 싶을 때 사용하는 함수
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
